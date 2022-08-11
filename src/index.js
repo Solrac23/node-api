f1b32fc27885200abdebafab93680cf70ea18ad1
Carlos Eduardo <eduardosantoos573@gmail.com>
2022-08-11 12:36:12 -0400

    update packages

--- src/index.js
+++ src/index.js
@@ -4,19 +4,17 @@ const cors       = require('cors')
 const requireDir = require('require-dir')
 const app        = express()
 
-app.use(express.json()) // permite receber a app receber json 
+app.use(express.json()) // permite receber a app receber json
 app.use(cors())
 mongoose.connect('mongodb+srv://Shiroe:4638554885@cluster0.bkdyf.mongodb.net/nodeApi?retryWrites=true&w=majority',{
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 	useFindAndModify: false
-}).catch(err => {
-	console.warn(err)
 })
 requireDir('./src/models')
 
 // o use recebe todo tipo de verbo http
-app.use('/api', require('./src/routes'))
+app.use('/api', require('./routes'))
 
 app.listen(3000, () => {
 	console.log('Server is running')
